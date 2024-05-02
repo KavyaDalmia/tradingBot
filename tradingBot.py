e39f3b74392cb8a520a1d55d15292fc2a05b5fc6
Kavya Dalmia <dalmia.kavya@gmail.com>
2024-05-02 10:46:31 -0400

    started to code the bot and import liabraries

---
+++ tradingBot.py
@@ -0,0 +1,8 @@
+from lumibot.brokers import Alpaca
+from lumibot.backtesting import YahooDataBacktesting
+from lumibot.strategies.strategy import Strategy
+from lumibot.traders import Trader
+from datetime import datetime
+
+API_KEY = ""
+SPI_SECRET = ""
