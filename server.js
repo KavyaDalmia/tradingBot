3277015a2858df5e59518cd47e555f92c64b2c35
Kavya Dalmia <dalmia.kavya@gmail.com>
2024-05-06 21:48:51 -0400

    server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const Alpaca = require("@alpacahq/alpaca-trade-api");
 const alpaca = new Alpaca();
 // server <==> data source
@@ -7,12 +8,16 @@ const WebSocket = require('ws');
 const wss = new WebSocket("wss://stream.data.alpaca.markets/v1beta1/news");
 const receive_Server = new WebSocket("ws://localhost:8080");
 dataToSend = ""
+probability = 0
+sentiment = "negative"
+symbol = ""
 console.log(receive_Server);
 
 function sendMsg() {
     if (dataToSend != "") {
         receive_Server.send(dataToSend);
     }
+
 };
 
 function loop() {
@@ -28,10 +33,26 @@ receive_Server.on('open', function open() {
     // loop()
 });
 
-receive_Server.on('message', function(data) {
-    console.log(data);
-    console.log(data.toString('utf-8'));
-    console.log('received something')
+receive_Server.on('message', async function(data) {
+    const dataArray = JSON.parse(data);
+    console.log(dataArray[0]); // Output: 0.8288981914520264
+    console.log(dataArray[1]); // Output: "negative"
+    probability = dataArray[0];
+    sentiment = dataArray[1];
+    console.log(symbol)
+    if (sentiment == "positive" && probability > 0.9) {
+        console.log('created order')
+        let order = await alpaca.createOrder({
+            symbol: symbol,
+            qty: 1,
+            side: "buy",
+            type: "market",
+            time_in_force: "day" // if the day ends then it wont trade
+        });
+    } else if (sentiment == "negative" && probability > 0.9) {
+        console.log('selling')
+        closedPosition = await alpaca.closePosition(symbol);
+    }
 });
 
 wss.on('open', function() {
@@ -39,8 +60,8 @@ wss.on('open', function() {
 
     const authMsg = {
         action: 'auth',
-        key: 'PK1B3494WJH3KWU0Y64L',
-        secret: 'mfvxDBtymMvsAL43L5xTJz75kgEqWSHA2bibwvrB'
+        key: 'PK28MFMJVX0XVJMY6SS8',
+        secret: 'kFAH2z0HhSPVBSw3dOGkfoSiRbhcWLM7DVKReoca'
     };
 
     wss.send(JSON.stringify(authMsg)); // send auth data tp ws log us in
@@ -60,12 +81,12 @@ wss.on('message', async function(message) {
     // message is string
     const currentEvent = JSON.parse(message)[0];
     if (currentEvent.T === "n") {
+        symbol = currentEvent.symbols[0]
         const headline = currentEvent.headline.toString();
         const summary = currentEvent.summary.toString();
         dataToSend = headline + summary
-        console.log(headline + summary)
-        sendMsg()
-        // python_web_socket.send(JSON.stringify({ action: 'invoke_function', headline: headline }));
+        console.log('data sent for' + symbol)
+        sendMsg();
     }
 });
 
